c1d6aa68dca111a888d42ccdd63a4a79fc1b668a
alumno <alumno>
2019-04-10 22:02:36 -0300

    tercera

---
+++ Clase_3.d
@@ -0,0 +1,18 @@
+. ->hace referencia al directorio actual.
+
+* ->hace referencia a todo lo siguiente.
+
+mv ->sirve para mover un directorio o renombrarlo.
+	mv /Origen/Archivo_Directorio /Destino
+	mv archivo archivo2 
+
+mkdir ->sirve para crear directorios.
+
+ln ->para establecer enlaces entre archivos.
+
+
+
+
+
+
+
